4dd3c4137b8b9d5bc9b3e0a96f809a3536941462
Dmitry Volodin <d.volodin@nekrasovka.ru>
2019-11-10 22:35:19 +0300

    Version 1.0

---
+++ funds.R
@@ -0,0 +1,85 @@
+## Main funds script
+
+# Needed packages
+packages <- c("tidyverse", "readtext", "lubridate")
+
+# Check whethet package is installed and attach it, If not installed - download, install and then attacj it
+for (p in packages) {
+    if (p %in% installed.packages()[, 1]) {
+        library(p)
+    } else {
+        install.packages(p)
+        library(p)
+    }
+}
+
+# Unzip the latest full update
+zipname <- system("ls /home/user/dumps", intern = TRUE)
+zipname <- zipname[which(
+    as.Date(str_sub(zipname, 1, 10), format = "%d.%m.%Y") == max(as.Date(str_sub(zipname, 1, 10), format = "%d.%m.%Y"), na.rm = TRUE)
+)]
+zipname <- zipname[str_detect(zipname, fixed("full.zip"))]
+unzip_command <- str_c("unzip /home/user/dumps/", zipname)
+system(unzip_command)
+
+# Path to source txt files (recently unzipped)
+txt_path <- system("ls /home/user/dumps", intern = TRUE)
+txt_path <- txt_path[which(
+    as.Date(str_sub(txt_path, 1, 10), format = "%d.%m.%Y") == max(as.Date(str_sub(txt_path, 1, 10), format = "%d.%m.%Y"), na.rm = TRUE)
+)]
+txt_path <- txt_path[str_detect(txt_path, fixed("full.txt"))]
+txt_path <- str_c("/home/user/dumps/", txt_path)
+
+# Path to resulting csv files
+csv_path <- str_c("/home/user/Comp_app/data/", c("funds_df_56.csv", "funds_df_5.csv"))
+
+# Loop over txt_path
+for (f in txt_path) {
+    # Read txt file as character vector of length 1
+    raw <- readtext(f, encoding = "UTF-8")$text
+
+    # Split raw by records
+    funds <- str_split(raw, "\n\n")[[1]]
+    rm(raw)
+    gc()
+    
+    # Split each record into lines (result: list element contents record, which contents character vector of fields)
+    funds <- map(funds, function(x) {str_split(x, "\n") %>% unlist()})
+
+    # Remove technical information about record
+    funds <- map(funds, function(x) x[-(1:2)])
+
+    # Remove last element of the last record (empty)
+    funds[length(funds)] <- list(funds[[length(funds)]][-length(funds[[length(funds)]])])
+
+    # Make character vector of ids (001 field)
+    ids <- map(funds, function(x) x[1]) %>% unlist() %>% str_sub(., 5, -1)
+
+    # Count how much id for each record must be repeated to make it key vector for the resulting dataframe
+    id_length <- (map(funds, length) %>% unlist()) - 1
+
+    # Repeat ids
+    ids <- rep(ids, id_length)
+    rm(id_length)
+    gc()
+
+    # Remove ids from each record
+    funds <- map(funds, function(x) x[-1])
+
+    # Extract field names (first three characters) and field content
+    field_names <- map(funds, str_sub, 1, 3) %>% unlist()
+    vars <- map(funds, str_sub, 5, -1) %>% unlist()
+    rm(funds)
+    gc()
+
+    # Make dataframe (first column - id of record, second column - name of the field in corresponding record, third column - content of corresponding field)
+    funds_df <- data.frame(
+        id = ids,
+        field = field_names,
+        var = vars
+    )
+    rm(ids, field_names, vars)
+
+    # Save dataframe
+    write_tsv(funds_df, csv_path[which(txt_path == f)])
+}
